3223bdd20c0f23d97cb46a0c0025efe4259d0e7a
Mark McDonnell <storm.mark@gmail.com>
2011-02-20 20:28:02 +0000

    swapped out CSS minification via Regular Expressions for YUI Compressor

--- Concat.php
+++ Concat.php
@@ -4,100 +4,53 @@
 		ob_start('ob_gzhandler');
 	}
 	
-	// Function that removes comments, white space and line breaks from a CSS file
-	function CSSCompress($subject) {
-		$subject = preg_replace('/\/\*([^*]|\*+[^\/*])*\*+\//', '', $subject); // Remove Comments
-		$subject = preg_replace('/(?<=[,;:{\'\/\n\r])\s+/i', '', $subject); // Remove White Space
-		$subject = preg_replace('/\s+(?=[^\w#!(.-])/i', '', $subject); // Remove odd left over cases of White Space (e.g. the gap inbetween err and the closeing { "b.err {...}")
-		$subject = preg_replace('/\n/', '', $subject); // Remove any line breaks so the entire file is in one line
-		return $subject;
-	}
-	
-	// Do not compress unless it's a CSS file we're dealing with
-	$compress = false;
-	
-	// Do not minify unless it's a JavaScript file we're dealing with
-	$minify = false;
-	
-	// Store reference to common data
+	// Store reference to querystring for faster access
 	$filelist = $_GET['files'];
-	$currentdate = gmdate('D, d M Y H:i:s \G\M\T', time());
 	
 	// Check for a valid file type
 	if (strtolower($_GET['filetype']) == 'js') {
 		$content_type = "javascript";
 		$extention = ".js";
-		
-		// Request Google Closure PHP Wrapper
-		include "YUICompressor.php";
-		$minify = true;
 	} elseif (strtolower($_GET['filetype']) == 'css') {
 		$content_type = "css";
 		$extention = ".css";
-		$compress = true;
 	} else {
 		die('An unknown file type was provided');
 	}
 	
+	// Request 'YUI Compressor' PHP Wrapper
+	include "YUICompressor.php";
+	
 	// Send the requisite header information and character set 
 	header ("content-type: text/$content_type; charset: UTF-8");
 	
-	// Initialize the 'compress' function to remove all comments and whitespace from the CSS files
-	// Also set the HTTP headers for the CSS files.
-	if ($compress) {
-		ob_start("CSSCompress");
-		
-		// Set expiration length to 6 months.
-		// This works out as... 
-		// 60 seconds * by 60 minutes which equals 1 hr
-		// then times the 1hr by 24 to get 1 day
-		// then times 1 day by however many days you want the item to be cached for (this case 30 days = practically 1 month)
-		// then times 1 month by how many months to cache by (in this case 6 months)
-		$expiration_length = 60 * 60 * 24 * 30 * 6;
-		$expiring = gmdate ("D, d M Y H:i:s", time() + $expiration_length);
-		
-		header('Last-Modified: ' . date ("F d Y H:i:s.", filemtime('Concat.php'))); 
-		header('Expires: ' . $expiring);
-		header("Cache-Control: max-age=" . $expiring . ", must-revalidate");
-		
-		// Grab each file and check they exist
-		$files = explode(",", $filelist);
-		
-		// List the files to be included
-		foreach ($files as $value) {
-			if (file_exists($value.$extention)) {
-				include $value.$extention;
-			}
+	// Grab each file and check they exist
+	$files = explode(",", $filelist);
+	$list = '';
+	$options = array('nomunge' => true, 'line-break' => 1000);
+	
+	// List the files to be included
+	foreach ($files as $value) {
+		if (file_exists($value.$extention)) {
+			$list .= file_get_contents($value.$extention);
 		}
 	}
 	
-	// Run processes JavaScript files via YUI Compiler Jar file
-	if ($minify) {
-		// Grab each file and check they exist
-		$files = explode(",", $filelist);
-		$scripts = '';
-		
-		// List the files to be included
-		foreach ($files as $value) {
-			if (file_exists($value.$extention)) {
-				$scripts .= file_get_contents($value.$extention);
-			}
-		}
-		
-		// Set-up paths
-		Minify_YUICompressor::$jarFile = $_SERVER['DOCUMENT_ROOT'].'/PHP/Mini-Com/yuicompressor-2.4.2.jar';
-	 	Minify_YUICompressor::$tempDir = $_SERVER['DOCUMENT_ROOT'].'/PHP/Mini-Com/tmp';
-		
-		// Store optimised code in variable
-	 	$code = Minify_YUICompressor::minifyJs(
-	 		$scripts
-	 		,array('nomunge' => true, 'line-break' => 1000)
-	 	);
-		
-		// Write the optimised code to the file
-		echo $code;
+	// Set-up paths
+	Minify_YUICompressor::$jarFile = $_SERVER['DOCUMENT_ROOT'].'/PHP/Mini-Com/yuicompressor-2.4.2.jar';
+ 	Minify_YUICompressor::$tempDir = $_SERVER['DOCUMENT_ROOT'].'/PHP/Mini-Com/tmp';
+	
+	// First check which YUI method to call (Js or Css)
+	// Then store the resulting 'minified' code in a variable
+	if ($content_type == 'javascript') {
+		$code = Minify_YUICompressor::minifyJs($list ,$options);
+	} else {
+		$code = Minify_YUICompressor::minifyCss($list, $options);
 	}
 	
+	// Now write the optimised code to the file
+	echo $code;
+		
 	if (extension_loaded('zlib')) {
 		ob_end_flush();
 	}
